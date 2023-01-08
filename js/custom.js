dd86586a242b9bf5d924251c44bff862b500b3a7
developeranil <prajapatianil1994@gmail.com>
2023-01-08 14:38:57 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    new WOW().init();
+
+    $("#home-slider").owlCarousel({
+      dots: false,
+      nav: true, // Show next and prev buttons
+      navText: ["arrow_back_ios", "arrow_forward_ios"],
+      autoplay: true,
+      loop: true,
+      items: 1,
+      itemsDesktop: false,
+      itemsDesktopSmall: false,
+      itemsTablet: false,
+      itemsMobile: false,
+    });
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop()) {
+        $("#back-top").fadeIn();
+      } else {
+        $("#back-top").fadeOut();
+      }
+    });
+})
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var stickyHeader = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
